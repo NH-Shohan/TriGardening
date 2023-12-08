1299e8c69d270bdff9305836a7ea51d8d1609b0b
NH-Shohan <nh.shohan6@gmail.com>
2023-12-09 02:03:08 +0600

    package.json file modified

--- backend/src/Auth/google.strategy.ts
+++ backend/src/Auth/google.strategy.ts
@@ -1,18 +1,21 @@
 // src/Auth/google.strategy.ts
-import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, VerifyCallback } from 'passport-google-oauth20';
 import { AuthService } from './auth.service';
-
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
   constructor(private readonly userService: AuthService) {
     super({
-      clientID: process.env.GOOGLE_CLIENT_ID || '565131900782-nuoge9aj5pondismpmg4i5qcom61dasq.apps.googleusercontent.com', // Replace with your actual Google Client ID
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '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', // Replace with your actual Google Client Secret
+      clientID:
+        process.env.GOOGLE_CLIENT_ID ||
+        '565131900782-nuoge9aj5pondismpmg4i5qcom61dasq.apps.googleusercontent.com', // Replace with your actual Google Client ID
+      clientSecret:
+        process.env.GOOGLE_CLIENT_SECRET ||
+        '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', // Replace with your actual Google Client Secret
       // google.strategy.ts
-callbackURL: 'http://localhost:3000/user/google/callback',
+      callbackURL: 'http://localhost:3000/user/google/callback',
 
       passReqToCallback: true,
       scope: ['profile', 'email'],
@@ -29,7 +32,11 @@ callbackURL: 'http://localhost:3000/user/google/callback',
     console.log('Google Profile:', profile);
     const { name, email, picture } = profile._json;
 
-    const user = await this.userService.findOrCreateGoogleUser(name, email, picture);
+    const user = await this.userService.findOrCreateGoogleUser(
+      name,
+      email,
+      picture,
+    );
 
     if (!user) {
       throw new UnauthorizedException('Unable to authenticate through Google');
--- backend/src/User/user.dto.ts
+++ backend/src/User/user.dto.ts
@@ -1,40 +1,44 @@
-import { IsEmail, IsIn, IsNotEmpty, IsOptional, IsString, Matches,MinLength, ValidateIf } from "class-validator";
-export class signUpDTO{
-   
-      @IsString({ message: 'enter name' })
-      @Matches(/^[a-zA-Z- ]+$/, { message: 'Name must be A_z' })
-      @IsNotEmpty()
-      name: string;
-    
-      @IsEmail({}, { message: 'enter a valid email' })
-      @IsOptional()
-      email: string;
-    
-      @IsNotEmpty()
-      @Matches(/^\d{11}$/, { message: 'Invalid mobile number format' })
-      @ValidateIf((object, value) => value !== undefined) // Validate only if value is provided
-      phone: string;
-    
-      @IsNotEmpty()
-      @MinLength(4, { message: 'Password is minimum 4 characters' })
-      password: string;
-      photo: string;
-    }
-   
-      
-      export class LoginDTO {
-        @IsOptional()
-        @IsNotEmpty()
-        @IsEmail({}, { message: 'Invalid email format' })
-        email?: string;
-      
-        @IsOptional()
-        @IsNotEmpty()
-        @Matches(/^\d{11}$/, { message: 'Invalid mobile number format' })
-        phone?: string;
-      
-        @IsNotEmpty()
-        password: string;
-      }
-      
-    
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Matches,
+  MinLength,
+  ValidateIf,
+} from 'class-validator';
+export class signUpDTO {
+  @IsString({ message: 'enter name' })
+  @Matches(/^[a-zA-Z- ]+$/, { message: 'Name must be A_z' })
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail({}, { message: 'enter a valid email' })
+  @IsOptional()
+  email: string;
+
+  @IsNotEmpty()
+  @Matches(/^\d{11}$/, { message: 'Invalid mobile number format' })
+  @ValidateIf((object, value) => value !== undefined) // Validate only if value is provided
+  phone: string;
+
+  @IsNotEmpty()
+  @MinLength(4, { message: 'Password is minimum 4 characters' })
+  password: string;
+  photo: string;
+}
+
+export class LoginDTO {
+  @IsOptional()
+  @IsNotEmpty()
+  @IsEmail({}, { message: 'Invalid email format' })
+  email?: string;
+
+  @IsOptional()
+  @IsNotEmpty()
+  @Matches(/^\d{11}$/, { message: 'Invalid mobile number format' })
+  phone?: string;
+
+  @IsNotEmpty()
+  password: string;
+}
