a334643a63c2a5dd39d281534e3d1e0c43323569
sheikhsumaya <sheikhsumaya622@gmail.com>
2024-02-04 21:03:58 +0600

    Footer

--- frontend/pages/layout.jsx
+++ frontend/pages/layout.jsx
@@ -1,5 +1,6 @@
 import { Glegoo } from "next/font/google";
 import Navbar from "../components/Navbar";
+import Footer from "@/components/Footer";
 
 const glegoo = Glegoo({ subsets: ["latin"], weight: ["400", "700"] });
 
@@ -9,6 +10,7 @@ export default function Layout({ children }) {
       <main className={`${glegoo.className}`}>
         <Navbar />
         {children}
+        <Footer></Footer>
       </main>
     </>
   );
