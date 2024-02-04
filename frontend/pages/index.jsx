6512b8569c261454773574b0740f7300ed5239db
sheikhsumaya <sheikhsumaya622@gmail.com>
2024-02-05 00:58:07 +0600

    card

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,9 +1,11 @@
+import HomeCard from "@/components/HomeCard";
 import HomeFirst from "@/components/HomeFirst";
 
 export default function Home() {
   return (
     <main className="">
       <HomeFirst></HomeFirst>
+      <HomeCard></HomeCard>
     </main>
   );
 }
