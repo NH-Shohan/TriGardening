2ee4a756d8bfbbfeb5f8708cf941d0aef2c1d0ac
Nahim Hossain Shohan <nh.shohan6@gmail.com>
2024-09-17 15:30:53 +0600

    restart from start

---
+++ client/src/app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="text-center max-w-7xl mx-auto">
+      <h1>
+        Harness the power of <span className="text-primary">gardening</span>
+      </h1>
+    </div>
+  );
+}
