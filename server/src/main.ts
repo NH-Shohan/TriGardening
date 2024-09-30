8ca7779dff94a644d6304727e94662f18e95a38b
Nahim Hossain Shohan <nh.shohan6@gmail.com>
2024-09-30 15:56:50 +0600

    password change implemented with client

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,15 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.use(cookieParser());
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
   app.setGlobalPrefix('api');
   const configService = app.get(ConfigService);
   await app.listen(configService.get('PORT') || 3333);
