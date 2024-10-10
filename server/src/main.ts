2359fc05a764c7bd00bcea303e3df503b961eb17
Nahim Hossain Shohan <nh.shohan6@gmail.com>
2024-10-10 16:52:52 +0600

    deploy

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,9 +7,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
+  const allowedOrigins = configService.get('ALLOWED_ORIGINS')?.split(',') || [
+    'https://trigardeningbd.vercel.app',
+    'http://localhost:3000',
+  ];
+
   app.use(cookieParser());
   app.enableCors({
-    origin: '*' || 'https://trigardeningbd.vercel.app',
+    origin: allowedOrigins,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
