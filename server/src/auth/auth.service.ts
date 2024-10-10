d3fc590fb6e695022aabf0a259da64f8b96e9829
Nahim Hossain Shohan <nh.shohan6@gmail.com>
2024-10-10 20:01:11 +0600

    re deploy

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -33,7 +33,7 @@ export class AuthService {
       res.cookie('access_token', token, {
         httpOnly: true,
         secure: true,
-        sameSite: 'none',
+        sameSite: 'lax',
         maxAge: 7 * 24 * 60 * 60 * 1000,
       });
 
